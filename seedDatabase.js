51b957e104b1e6db095d81b3faa2ac6e63d9d898
mattman240 <mattmull98@gmail.com>
2018-03-14 18:37:26 -0700

    fixed file system and duplicate errors

--- seedDatabase.js
+++ seedDatabase.js
@@ -1,14 +1,15 @@
 const db = require('./db/dbHelpers');
 const sampleData = require('./data/sampleData');
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/silverspoon';
 
-mongoose.connect(MONGO_URI);
+mongoose.connect('mongodb://localhost/silverspoon');
 
-db.save({ data: sampleData, model: db.Restaurant }, (result) => {
-  if (result) {
-    console.log('all added to db');
-    mongoose.disconnect();
-  }
-});
+// db.save({ data: sampleData, model: db.Restaurant }, (result) => {
+//   if (result) {
+//     console.log('all added to db');
+//     mongoose.disconnect();
+//   }
+// });
