895f5aad50bef1839c876dd9647065e93a957881
mattman240 <mattmull98@gmail.com>
2018-03-14 18:47:04 -0700

    removed unused npm packages

--- seedDatabase.js
+++ seedDatabase.js
@@ -1,15 +1,7 @@
 const db = require('./db/dbHelpers');
 const sampleData = require('./data/sampleData');
 const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/silverspoon';
 
 mongoose.connect('mongodb://localhost/silverspoon');
-
-// db.save({ data: sampleData, model: db.Restaurant }, (result) => {
-//   if (result) {
-//     console.log('all added to db');
-//     mongoose.disconnect();
-//   }
-// });
